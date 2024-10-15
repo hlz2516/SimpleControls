93f0560332ea735556b7fcb5cb11b841f988d53a
hlz2516 <714251494@qq.com>
2024-10-15 10:02:36 +0800

    完成checkedcombobox1

---
+++ SimpleControls/SimpleControls/CheckedComboBox.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows;
+
+namespace SimpleControls
+{
+    /// <summary>
+    /// CheckedComboBox.xaml 的交互逻辑
+    /// </summary>
+    public partial class CheckedComboBox : UserControl
+    {
+        private List<string> selectedResult = new List<string>();
+        public char Separator { get; set; } = ',';
+
+        public CheckedComboBox()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            var chk = (CheckBox)sender;
+            //检查选择列表中是否包含当前选中的词，如果不包含，则追加
+            if (chk.Content != null)
+            {
+                string text = chk.Content.ToString();
+                if (!selectedResult.Contains(text))
+                {
+                    selectedResult.Add(text);
+                }
+                UpdateResult();
+            }
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            var chk = (CheckBox)sender;
+            //检查选择列表中是否包含当前选中的词，如果包含，则删除
+            if (chk.Content != null)
+            {
+                string text = chk.Content.ToString();
+                if (selectedResult.Contains(text))
+                {
+                    selectedResult.Remove(text);
+                }
+                UpdateResult();
+            }
+        }
+
+        private void UpdateResult()
+        {
+            tb.Content = string.Join(Separator, selectedResult);
+        }
+
+        private void CheckBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            var chk = (CheckBox)sender;
+            var border = chk.Parent as Border;
+            border.Background = (LinearGradientBrush)FindResource("ComboBox.MouseOver.Editable.Button.Background");
+            border.BorderBrush = (SolidColorBrush)FindResource("ComboBox.MouseOver.Editable.Button.Border");
+        }
+
+        private void CheckBox_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var chk = (CheckBox)sender;
+            var border = chk.Parent as Border;
+            border.BorderBrush = new SolidColorBrush(Colors.Transparent);
+            border.Background = new SolidColorBrush(Colors.Transparent);
+        }
+
+        public IEnumerable ItemSource
+        {
+            get { return (IEnumerable)GetValue(ItemSourceProperty); }
+            set
+            {
+                SetValue(ItemSourceProperty, value);
+                items.ItemsSource = value;
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for ItemSourceProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemSourceProperty =
+            DependencyProperty.Register("ItemSource", typeof(IEnumerable), typeof(CheckedComboBox), new PropertyMetadata(new PropertyChangedCallback(ItemSourceChanged)));
+
+        private static void ItemSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var cb = d as CheckedComboBox;
+            cb.items.ItemsSource = (IEnumerable)e.NewValue;
+        }
+    }
+}
--- SimpleControls/WPF_Demo/MainWindow.xaml.cs
+++ SimpleControls/WPF_Demo/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,66 +20,23 @@ namespace WPF_Demo
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<string> selectedResult = new List<string>();
-        private char Separator = ',';
         public MainWindow()
         {
             InitializeComponent();
-            var fruits = new List<string>
+            var fruits = new ObservableCollection<string>
             {
                 "苹果","橘子","香蕉"
             };
-            items.ItemsSource = fruits;
+            //cb.ItemSource = fruits;
+            //cb2.ItemsSource = fruits;
+            var vm = new ViewModel();
+            vm.Fruits = fruits;
+            this.DataContext = vm;
         }
+    }
 
-        private void tb_Checked(object sender, RoutedEventArgs e)
-        {
-            System.Diagnostics.Debug.WriteLine("tb_Checked");
-        }
-
-        private void tb_Unchecked(object sender, RoutedEventArgs e)
-        {
-            System.Diagnostics.Debug.WriteLine("tb_Unchecked");
-        }
-
-        private void tb_Click(object sender, RoutedEventArgs e)
-        {
-           
-        }
-
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-            var chk = (CheckBox)sender;
-            //检查选择列表中是否包含当前选中的词，如果不包含，则追加
-            if (chk.Content != null)
-            {
-                string text = chk.Content.ToString();
-                if (!selectedResult.Contains(text))
-                {
-                    selectedResult.Add(text);
-                }
-                UpdateResult();
-            }
-        }
-
-        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
-        {
-            var chk = (CheckBox)sender;
-            //检查选择列表中是否包含当前选中的词，如果包含，则删除
-            if (chk.Content != null)
-            {
-                string text = chk.Content.ToString();
-                if (selectedResult.Contains(text))
-                {
-                    selectedResult.Remove(text);
-                }
-                UpdateResult();
-            }
-        }
-
-        private void UpdateResult()
-        {
-            tb.Content = string.Join(Separator, selectedResult);
-        }
+    public class ViewModel
+    {
+        public ObservableCollection<string> Fruits { get; set; }
     }
 }
