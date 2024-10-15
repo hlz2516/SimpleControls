4b565e0603d974af639e72d6edc26ff9961aaa27
hlz2516 <714251494@qq.com>
2024-10-15 10:02:20 +0800

    完成CheckedComboBox

--- SimpleControls/WPF_Demo/obj/Debug/net6.0-windows/WPF_Demo.AssemblyInfo.cs
+++ SimpleControls/WPF_Demo/obj/Debug/net6.0-windows/WPF_Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPF_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d72794306ced16f34358a1301eecc751aee6c791")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPF_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPF_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
